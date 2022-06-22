7f0e8840bc3127d934312d597f6568f2feda8d5f
NickViskr <nviskr@gmail.com>
2022-06-22 23:51:50 +0500

    Switching implemented through a case

--- Traffic light 2/ViewController.swift
+++ Traffic light 2/ViewController.swift
@@ -15,18 +15,56 @@ class ViewController: UIViewController {
     
     @IBOutlet var switchButton: UIButton!
     
-    private var clickCounter = 0
+    private var currentLight = CurrentLight.red
+    private let lightIsOff: CGFloat = 0.2
+    private let lightIsOn: CGFloat = 1
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         switchButton.layer.cornerRadius = 15
         
-    }
-    
-    @IBAction func switchButton(_ sender: Any) {
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
         
     }
     
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = redLight.frame.width / 2
+        greenLight.layer.cornerRadius = redLight.frame.width / 2
+        
+    }
+    
+    @IBAction func switchButtonTap() {
+        if switchButton.currentTitle == "START" {
+            switchButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            greenLight.alpha = lightIsOn
+            yellowLight.alpha = lightIsOff
+            currentLight = .red
+        }
+    }
+    
+    
+}
+
+// MARK: - CurrentLight
+extension ViewController {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
 }
 
